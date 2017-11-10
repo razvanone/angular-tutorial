fe527ca7b538e17e2d9bbfe6a899ab9a102a9b8f
Razvan Mihailescu <rmihailescu@cbn.ro>
2017-11-10 18:49:04 +0200

    Adding external service calls support. First steps.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   declarations: [
     AppComponent,
